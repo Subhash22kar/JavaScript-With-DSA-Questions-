003e65a0db86061a095b5cb731dbca76496deaf7
Subhash Baviskar <0126it201107@oriental.ac.in>
2025-12-23 09:30:12 +0000

    Stack

--- STACKS/stack.js
+++ STACKS/stack.js
@@ -1,1 +1,30 @@
-console.log("subhash");
+// STACKS!
+
+// function: push, pop, peek, lenght
+
+var letters = []; // this is our stack
+
+var word = "racecar"
+
+var reword = "";
+
+// put letters of word into stack
+
+for (var i = 0; i < word.length; i++){
+    letters.push(word[i]);
+}
+
+// pop off the stack in reverse order
+
+for (let i = 0; i < word.length; i++) {
+    reword += letters.pop();
+    
+}
+
+if(reword === word){
+    console.log(word + "is a palindrome");
+    
+}
+else{
+    console.log(word + "is not a palindrone.")
+}
