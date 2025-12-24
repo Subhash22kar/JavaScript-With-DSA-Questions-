448139c83148eccc63d06650cb7549e0463821c6
Subhash Baviskar <0126it201107@oriental.ac.in>
2025-12-24 16:12:37 +0530

    array basic understanding

---
+++ 01_arr1.js
@@ -0,0 +1,22 @@
+// let arr = [];
+// arr[0] = 100;
+// arr[1] = 200;
+// arr[2] = 300;
+
+
+// arr[7] = 700;
+
+// console.log(arr);
+
+let arr = new Array(3);
+
+// arr[0] = 10;
+// arr[1] = 20;
+// arr[2] = 30;
+// arr[3] = 40;
+
+
+// arr[10] = 500;
+
+arr.push(1000);
+console.log(arr);
