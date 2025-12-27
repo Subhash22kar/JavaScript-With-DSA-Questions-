20eae0e4fba95884b958fa3ba1d8df6413071e4e
Subhash Baviskar <0126it201107@oriental.ac.in>
2025-12-27 20:57:30 +0530

    (Shop discount) Show Payble amount

---
+++ Conditional_Stament/02_conditional.js
@@ -0,0 +1,41 @@
+
+
+// Problem 2 is Shop discount
+
+let amount = Number(prompt(" What is the final amount ?"));
+
+let  = dis = 0;
+
+// if(amount > 0 &&  amount <= 5000) {
+//     console.log(amount);
+    
+// } else if(amount > 5000 &&  amount <= 7000) {
+//     console.log(amount - Math.floor((5*amount)/100));
+    
+// } else if(amount > 7000 &&  amount <= 9000) {
+//     console.log(amount - Math.floor((10*amount)/100));
+    
+// } else if(amount > 9000) {
+//     console.log(amount - Math.floor((20*amount)/100));
+    
+// } else {
+//     console.log("Wrong input");
+    
+// }
+
+if(amount > 0 &&  amount <= 5000) {
+  dis = 0;
+} else if(amount > 5000 &&  amount <= 7000) {
+    dis = 5;
+    
+} else if(amount > 7000 &&  amount <= 9000) {
+    dis = 10;
+    
+} else if(amount > 9000) {
+    dis = 20;
+} else {
+    console.log("Wrong input");
+    
+}
+
+ console.log(amount - Math.floor((dis*amount)/100));
