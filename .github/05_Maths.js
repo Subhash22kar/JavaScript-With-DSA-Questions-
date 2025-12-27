b6e5e7ca4f0d05f4009c3d896ae96130d628c9f8
Subhash Baviskar <0126it201107@oriental.ac.in>
2025-12-27 16:58:05 +0530

    array

---
+++ .github/05_Maths.js
@@ -0,0 +1,45 @@
+
+// Math Function
+
+// console.log(Math.round(11.3));
+// console.log(Math.ceil(11.1));
+// console.log(Math.floor(11.7));
+
+// console.log(Math.trunc(10.98)); // remove the decimal part
+
+// console.log(Math.pow(2,5)); // 2^5
+
+// console.log(Math.sqrt(25)); //5
+
+// console.log(Math.cbrt(2)); // 1.25999
+
+// console.log(Math.abs(-15));
+// console.log(Math.max(11, 100, 200, 3, 5, ));
+// console.log(Math.min(11, 100, 200, 3, 5, ));
+
+// let a = 89.34343;
+
+// console.log(a.toFixed(4));
+
+
+//*************************************************Math Problems */
+
+// console.log(Math.trunc(Math.random()*9000)+1000); // 0 to 999
+
+
+let a = 5;
+let b = 7;
+
+console.log(a*b);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Basics/03_basic.js
@@ -0,0 +1,12 @@
+
+// Arithmetic Operators + , - , %, /
+let  a = 113;
+
+
+// console.log(Math.floor(a/b));
+
+// console.log(a%b);
+
+console.log(Math.floor(a/10));
+
+
