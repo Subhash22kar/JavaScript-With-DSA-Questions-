ff8a310acac533287ed1e902d81689302a96d315
Subhash Baviskar <0126it201107@oriental.ac.in>
2025-12-27 17:42:11 +0530

    Problems

---
+++ Basics/06_Math_Problems.js
@@ -0,0 +1,43 @@
+//*************************************************  Math Problems *****************************************
+
+//***  Problem 1 genrete OTP
+
+// console.log(Math.trunc(Math.random()*9000)+1000); // 0 to 999
+
+//***  Problem 2 Calculate area and perimeter of ractangle // formula is 2*lenght + Brith
+
+
+// let a = 5;
+// let b = 7;
+
+// console.log(a*b); // area
+
+// console.log(2*(a+b)); // perimeter of ractangle
+
+
+//*** Problem 3 area of triangle by heron's formula
+ 
+// sqrt of  s * (s-a) * (s-a) * (s-a)
+
+
+// let  a = 5;
+
+// let b =  4;
+
+// let  c = 3;
+
+// let s = (a+b+c)/2
+
+// console.log(s);
+
+// console.log(Math.sqrt(s * (s-a) * (s-b) * (s-c)));
+
+
+//*** Problem 4 Circumference of  circle
+
+
+let  r = 12;
+
+console.log(Number((2*Math.PI*r).toFixed(2)));
+
+
