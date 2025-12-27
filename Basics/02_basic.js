92761b0fae208d4fdb7b8d144f4ce1e1740793df
Subhash Baviskar <0126it201107@oriental.ac.in>
2025-12-27 17:00:44 +0530

    math

---
+++ Basics/02_basic.js
@@ -0,0 +1,47 @@
+
+// let a = 10;
+
+// let b = 20;
+
+// First Method
+
+// let c;
+
+// c = a;
+// a = b;
+// b = c;
+
+// console.log(a);
+// console.log(b);
+
+
+// Second Method
+
+
+// let a = 10;
+
+// let b = 20;
+
+// a = a + b; // a = 30
+
+// b = a - b; // b = 10
+
+// a = a - b //  a = 20
+
+// console.log(a,b);
+
+
+// Third Method
+
+let a = 10;
+
+let b = 20;
+
+[a,b] = [b,a]
+
+console.log(a,b);
+
+
+
+
+
