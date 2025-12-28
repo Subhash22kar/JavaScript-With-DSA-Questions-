dea870053d8fe02a403fbe063bb0e0eb327e2597
Subhash Baviskar <0126it201107@oriental.ac.in>
2025-12-28 17:53:17 +0530

    Loop

---
+++ Loop/01_for_loop.js
@@ -0,0 +1,42 @@
+
+// Loop 
+
+// for, while , do- while
+
+// 1 - 22
+
+// for(i = 1; i<23; i++){
+
+//     console.log(i);
+    
+// }
+
+
+// 23 - 54
+
+// for(i = 23; i<55; i++){
+//     console.log(i);
+    
+// }
+
+
+// 200 - 100
+
+// for( i = 200; i>99; i--){
+
+//     console.log(i);
+    
+// }
+
+
+// 1- 10
+
+let = 1;
+
+for(i = 1; i<=10; i++){
+
+    console.log(i);
+    
+}
+
+console.log(i + " Loop is terminate ");
