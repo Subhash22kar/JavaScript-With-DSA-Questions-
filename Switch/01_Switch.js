b665ab95a357a162ea7c1385e513dcd3841d9293
Subhash Baviskar <0126it201107@oriental.ac.in>
2025-12-28 16:50:58 +0530

    Swich case

---
+++ Switch/01_Switch.js
@@ -0,0 +1,70 @@
+
+// let day = 2;
+
+// switch(day){
+        
+//     case 1: console.log("monday");
+
+//     break
+    
+
+//     case 2: console.log("tuesday");
+//     break
+
+//     default : console.log("Invalid");
+// }
+
+// let day = 5;
+// switch(day){
+
+//     case 1:
+//     case 2:
+//     case 3:
+//   console.log("Tuesday");
+//    break
+    
+
+//      case 4:
+//     case 5:
+//     case 6:
+//   console.log("wedneday");
+//    break
+
+//    default: console.log("Invalid");
+   
+  
+
+// }
+
+
+// switch(true){
+
+//     case 13>12:
+//         console.log("hello");
+//         break
+
+//         case 13<10:
+//             console.log("hey");
+
+//             default : console.log("Invalid INPUT");
+                   
+// }
+
+let num  = 0.1 + 0.2;
+
+switch(num){
+
+    case 0.3: console.log("hello");
+    break
+
+    case 0.5: console.log("hey");
+    break
+
+    default: console.log("namasate");
+    
+     
+
+    
+
+
+}
