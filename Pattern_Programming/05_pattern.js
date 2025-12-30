90757a8d1750f5b67c96f8c8c654610ca1f51939
Subhash Baviskar <0126it201107@oriental.ac.in>
2025-12-30 18:46:35 +0530

    question 5

---
+++ Pattern_Programming/05_pattern.js
@@ -0,0 +1,21 @@
+const prompt = require("prompt-sync")();
+
+let n = Number(prompt("Enter a Number "))
+
+for(let i = 1; i<=n; i++){
+    
+    for (let j = 1; j<=n-i+1; j++) {
+        process.stdout.write("* ")   
+    }
+    console.log();
+    
+
+}
+
+// question 5
+
+// * * * * * 
+// * * * *
+// * * *
+// * *
+// *
