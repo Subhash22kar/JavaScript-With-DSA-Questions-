6e2e5800438fccfd83bb422ec374adae292ad6fb
Subhash Baviskar <0126it201107@oriental.ac.in>
2025-12-30 15:53:02 +0530

    Pattern

---
+++ Pattern_Programming/01_pattern.js
@@ -0,0 +1,56 @@
+
+const  prompt = require("prompt-sync")();
+// console.log("Hello");
+// console.log("World");
+
+// process.stdout.write("hello ")
+// process.stdout.write("world")
+
+
+// let number = prompt("enter a number ")
+
+// console.log(number);
+
+// console.log("*");
+// console.log("*");
+// console.log("*");
+// console.log("*");
+// console.log("*");
+// console.log("*");
+
+
+// process.stdout.write("*")
+// process.stdout.write("*")
+// process.stdout.write("*")
+// process.stdout.write("*")
+// process.stdout.write("*")
+
+
+let n = Number(prompt("Enter a number  "))
+
+for(let i = 1; i<=n; i++){
+
+    process.stdout.write("* ")
+}
+console.log();
+for(let i = 1; i<=n; i++){
+
+    process.stdout.write("* ")
+}
+console.log();
+for(let i = 1; i<=n; i++){
+
+    process.stdout.write("* ")
+}
+console.log();
+for(let i = 1; i<=n; i++){
+
+    process.stdout.write("* ")
+}
+console.log();
+for(let i = 1; i<=n; i++){
+
+    process.stdout.write("* ")
+}
+console.log();
+
