d2501e369a693ef377353e5d03b0d3575987b629
Subhash Baviskar <0126it201107@oriental.ac.in>
2026-01-02 14:25:58 +0530

    Right rotation by k element  without extra space or time complexity n

---
+++ Array/09_arr.js
@@ -0,0 +1,26 @@
+
+
+// Right rotation by k element without extar space or time complexity n
+
+let arr = [1,2,3,4,5]
+let k = Number(prompt("Enter a value"))
+k = k % arr.length
+reverse(0, arr.length-1)
+reverse(0, k-1)
+reverse(k, arr.length-1)
+
+function reverse(i, j){
+    while(i<j){
+
+        let temp = arr[i]
+        arr[i] = arr[j]
+         
+        arr[j] = temp
+
+        i++
+        j--
+
+    }
+}
+
+console.log(arr);
