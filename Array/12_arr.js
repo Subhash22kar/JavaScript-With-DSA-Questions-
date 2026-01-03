25094979be4f1990b04c23f3659086537355a58d
Subhash Baviskar <0126it201107@oriental.ac.in>
2026-01-03 23:19:41 +0530

    Leetcode question Best time to by and sell stocks

---
+++ Array/12_arr.js
@@ -0,0 +1,14 @@
+
+// Best time  to buy  and sell stocks
+
+var maxProfit = function(prices) {
+    let maxProfit = 0;
+    let min = prices[0];
+    for(let i = 0; i<prices.length; i++){
+        if(prices[i]<min) min = prices[i]
+        let profit = prices[i] - min;
+        maxProfit = Math.max(maxProfit, profit)
+    }
+
+    return maxProfit;
+}
