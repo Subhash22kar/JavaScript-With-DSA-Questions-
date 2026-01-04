0535f3481509a4ed4adbfb82b8d8f0857ab8fa0f
Subhash Baviskar <0126it201107@oriental.ac.in>
2026-01-04 15:51:27 +0530

    Maximum Subarray

--- Array/14_arr.js
+++ Array/14_arr.js
@@ -1,3 +1,19 @@
 
 
 // Maximum Subarry / kadane's algorithm // Leetcode Question
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+
+    let max = -Infinity;
+    let sum = 0;
+    for(let i = 0; i<nums.length; i++){
+        sum += nums[i]
+        max = Math.max(max, sum)
+        if(sum<0) sum =0;
+    }
+    return max;  
+};
