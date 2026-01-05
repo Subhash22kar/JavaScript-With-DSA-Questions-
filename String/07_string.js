6844427fd3f907f0d0f9fa60fc47705c3da343d9
Subhash Baviskar <0126it201107@oriental.ac.in>
2026-01-05 16:47:24 +0530

    Question 07 frequency of each character

---
+++ String/07_string.js
@@ -0,0 +1,27 @@
+//  frequency of each character
+
+// hello
+// h = 1
+// e = 1
+// l = 2
+// o = 1
+
+let s = prompt("Enter a string");
+
+let arr = new Array(128).fill(0);
+
+for(let i = 0; i<s.length; i++){
+
+    let index = s.charCodeAt(i);
+
+    arr[index] = arr[index] + 1;
+}
+
+for(let i = 0; i<arr.length; i++){
+
+    if(arr[i]>0){
+        console.log(String.fromCharCode(i) + "  appears at " + arr[i] + " times ");
+        
+    }
+}
+
