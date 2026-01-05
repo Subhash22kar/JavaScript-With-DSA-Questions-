3fb8f50aa7f3100b86975ffc24817294d215de28
Subhash Baviskar <0126it201107@oriental.ac.in>
2026-01-05 16:11:58 +0530

    Question 6 Toggle each character

---
+++ String/06_string.js
@@ -0,0 +1,21 @@
+// Questoin 6 Toggle each character
+
+// "DaDSaEFDq"
+
+let s = prompt("Enter a string");
+
+let toggle = "";
+
+console.log(s);
+
+for (let i = 0; i < s.length - 1; i++) {
+  let ch = s.charCodeAt(i);
+
+  if (ch >= 65 && ch <= 99) {
+    toggle = toggle + String.fromCharCode(ch + 32);
+  } else if (ch >= 97 && ch <= 122) {
+    toggle = toggle + String.fromCharCode(ch - 32);
+  }
+}
+
+console.log(toggle);
