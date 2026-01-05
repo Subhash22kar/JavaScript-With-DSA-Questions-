c623c1d902b6ecb8d7a4046543eb61633c4fb9ee
Subhash Baviskar <0126it201107@oriental.ac.in>
2026-01-05 14:04:25 +0530

    Print each character on new line

---
+++ String/01_string.js
@@ -0,0 +1,47 @@
+
+// let name = "Subhash";
+
+// let num = 23;
+
+// console.log(name);
+// console.log(num);
+
+
+// let arr = [10, 20, 30, 40];
+
+// arr[2] = 100
+
+// console.log(arr);
+
+// let name = "SUBHASH";
+
+// name.push("12")
+
+// console.log(name[5]);
+
+// let s = "Subhash"
+let s = "Subhash"
+
+// console.log(s.slice(-4,s.length));
+// console.log(s.substring(7));
+// console.log(s.toUpperCase());
+// console.log(s.concat(" ", "Wrold" , " ", "Duniya"));
+// console.log(s.trim());
+// console.log(s[2]);
+// console.log(s.charAt(2));
+// console.log(s.charCodeAt(2));
+
+
+// let arr = [10, 20, 30,40,50]
+
+
+// Question 1 Print each charater on new line
+
+for(let i = 0; i<s.length; i++){
+    console.log(s[i]);
+    
+}
+
+
+
+
