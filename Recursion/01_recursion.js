793fd0a86597b78d45f8461c1268625afe029ea8
riteshwadhwani <riteshwadhwani05@gmail.com>
2026-01-10 21:46:28 +0530

    changed cdn

--- Recursion/01_recursion.js
+++ Recursion/01_recursion.js
@@ -1,7 +1,7 @@
-// Print "hello World" 10 times using recursion
+// Print "hello World" 11 times using recursion
 
 function  temp(n){
-   if(n == 0) return;
+   if(n < 0) return;
     console.log("hello World");
     temp(n-1);
     
